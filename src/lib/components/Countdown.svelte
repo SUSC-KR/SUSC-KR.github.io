f0793bb688d7046320d4299214a2dfa7f59213e9
cpprhtn <cpprhtn@naver.com>
2023-07-11 14:15:47 +0900

    fix: mobile UI bug

--- src/lib/components/Countdown.svelte
+++ src/lib/components/Countdown.svelte
@@ -1,27 +1,27 @@
 <script>
-	import { onMount } from 'svelte';
-  
-	let days = 0;
-	let hours = 0;
-	let minutes = 0;
-	let seconds = 0;
-  
-	onMount(() => {
-	  const targetDate = new Date(2023, 6, 15, 13, 30); // 2023년 7월 15일 오후 1시 30분
-	  const countdownInterval = setInterval(() => {
-		const now = new Date();
-		const timeDifference = targetDate - now;
-  
-		if (timeDifference > 0) {
-		  days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-		  hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		  minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-		  seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-		} else {
-		  clearInterval(countdownInterval);
-		}
-	  }, 1000);
-	});
+  import { onMount } from 'svelte';
+
+let days = "00";
+let hours = "00";
+let minutes = "00";
+let seconds = "00";
+
+onMount(() => {
+  const targetDate = new Date(2023, 6, 15, 13, 30); // 2023년 7월 15일 오후 1시 30분
+  const countdownInterval = setInterval(() => {
+	const now = new Date();
+	const timeDifference = targetDate - now;
+
+	if (timeDifference > 0) {
+	  days = String(Math.floor(timeDifference / (1000 * 60 * 60 * 24))).padStart(2, '0');
+	  hours = String(Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, '0');
+	  minutes = String(Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, '0');
+	  seconds = String(Math.floor((timeDifference % (1000 * 60)) / 1000)).padStart(2, '0');
+	} else {
+	  clearInterval(countdownInterval);
+	}
+  }, 1000);
+});
   </script>
   
   <style>
@@ -36,10 +36,9 @@
 	  display: flex;
 	  flex-direction: column;
 	  align-items: center;
-	  /* background-color: #f0f0f0; */
 	  border-radius: 10px;
 	  padding: 15px;
-	  /* color: #333; */
+	  min-width: 72px;
 	}
   
 	.countdown-value {
@@ -52,21 +51,21 @@
 	}
   </style>
 
-<div class="countdown-container">
+<div class="countdown-container flex center">
 	<div class="countdown-item bg-neutral font-mono">
-	  <span class="countdown-value"><span style="--value: {days};">{days}</span></span>
+	  <span class="countdown-value">{days}</span>
 	  <span class="countdown-label">days</span>
 	</div>
 	<div class="countdown-item bg-neutral">
-	  <span class="countdown-value"><span style="--value: {hours};">{hours}</span></span>
+	  <span class="countdown-value">{hours}</span>
 	  <span class="countdown-label">hours</span>
 	</div>
 	<div class="countdown-item bg-neutral">
-	  <span class="countdown-value"><span style="--value: {minutes};">{minutes}</span></span>
+	  <span class="countdown-value">{minutes}</span>
 	  <span class="countdown-label">min</span>
 	</div>
 	<div class="countdown-item bg-neutral">
-	  <span class="countdown-value"><span style="--value: {seconds};">{seconds}</span></span>
+	  <span class="countdown-value">{seconds}</span>
 	  <span class="countdown-label">sec</span>
 	</div>
-  </div>
+</div>
