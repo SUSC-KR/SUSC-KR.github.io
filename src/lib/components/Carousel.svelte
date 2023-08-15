14977bf0542dc94ab61e56119e93d3dc0f8a95b0
cpprhtn <cpprhtn@naver.com>
2023-08-15 13:47:40 +0900

    feat: 성과공유회

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,44 +1,42 @@
-<!-- <div class="carousel carousel-center gap-10 -mt-[10rem] lg:-mt-[10rem] py-10">
+<div class="text-4xl font-bold text-center my-5">2023 Summer 성과공유회</div>
+<div class="carousel carousel-center gap-10 py-10">
 	<div class="carousel-item hover:scale-[102%] transition-all duration-300">
-		<img
-			class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
-			src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
-			alt="Burger"
-		/>
+		<video
+				class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
+				src="/flutter.mp4"
+				autoplay
+				loop
+				muted>
+		</video>
 	</div>
+
 	<div class="carousel-item hover:scale-[102%] transition-all duration-300">
-		<img
-			class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
-			src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
-			alt="Burger"
-		/>
+		<video
+				class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
+				src="/flutter.mp4"
+				autoplay
+				loop
+				muted>
+		</video>
 	</div>
+
 	<div class="carousel-item hover:scale-[102%] transition-all duration-300">
-		<img
-			class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
-			src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
-			alt="Burger"
-		/>
+		<video
+				class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
+				src="/flutter.mp4"
+				autoplay
+				loop
+				muted>
+		</video>
 	</div>
+
 	<div class="carousel-item hover:scale-[102%] transition-all duration-300">
-		<img
-			class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
-			src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
-			alt="Burger"
-		/>
+		<video
+				class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
+				src="/flutter.mp4"
+				autoplay
+				loop
+				muted>
+		</video>
 	</div>
-	<div class="carousel-item hover:scale-[102%] transition-all duration-300">
-		<img
-			class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
-			src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
-			alt="Burger"
-		/>
-	</div>
-	<div class="carousel-item hover:scale-[102%] transition-all duration-300">
-		<img
-			class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
-			src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
-			alt="Burger"
-		/>
-	</div>
-</div> -->
+</div>
