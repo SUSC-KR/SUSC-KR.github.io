0af5d005f045fb59cbfef9d7e9df24e61488a836
cpprhtn <cpprhtn@naver.com>
2023-08-16 01:36:14 +0900

    fix: mobile stats view

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,5 +1,5 @@
 <div class="text-4xl font-bold text-center my-5">2023 Summer<br>성과공유회</div>
-<div class="carousel carousel-center gap-10 py-10">
+<div class="carousel carousel-center gap-10 py-20">
 	<div class="carousel-item hover:scale-[102%] transition-all duration-300">
 		<video
 				class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
