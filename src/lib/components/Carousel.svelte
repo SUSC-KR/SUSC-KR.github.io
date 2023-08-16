deef110579a0e799a660d64cd1bdb4a4c7239b3b
cpprhtn <cpprhtn@naver.com>
2023-08-16 18:19:12 +0900

    feat 성과공유회

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,44 +1,35 @@
-<div class="text-4xl font-bold text-center my-5">2023 Summer<br>성과공유회</div>
-<div class="carousel carousel-center gap-10 py-20">
-	<div class="carousel-item hover:scale-[102%] transition-all duration-300">
-		<video
-				class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
-				src="/flutter.mp4"
-				autoplay
-				loop
-				muted
-				playsinline>
-		</video>
-	</div>
+<div class="text-4xl font-bold text-center my-5"><br>2023 Summer<br>성과공유회</div>
+<div class="carousel carousel-center flex gap-10 py-10 scroll-smooth">
+    <div class="carousel-item contents hover:scale-[102%] transition-all duration-300">
+        <video
+                class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
+                src="/flutter.mp4"
+                autoplay
+                loop
+                muted
+                playsinline>
+        </video>
+    </div>
 
-	<div class="carousel-item hover:scale-[102%] transition-all duration-300">
-		<video
-				class="w-[52%] sm:w-[52%] lg:w-[52%] xl:w-[52%] rounded-lg shadow-lg"
-				src="/spring.mp4"
-				autoplay
-				loop
-				muted
-				playsinline>
-		</video>
-	</div>
+    <div class="carousel-item contents hover:scale-[102%] transition-all duration-300">
+        <video
+                class="w-[100%] sm:w-[52%] lg:w-[52%] xl:w-[52%] rounded-lg shadow-lg"
+                src="/spring.mp4"
+                autoplay
+                loop
+                muted
+                playsinline>
+        </video>
+    </div>
 
-	<!-- <div class="carousel-item hover:scale-[102%] transition-all duration-300">
-		<video
-				class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
-				src="/flutter.mp4"
-				autoplay
-				loop
-				muted>
-		</video>
-	</div>
-
-	<div class="carousel-item hover:scale-[102%] transition-all duration-300">
-		<video
-				class="w-48 sm:w-56 lg:w-64 xl:w-72 rounded-lg shadow-lg"
-				src="/flutter.mp4"
-				autoplay
-				loop
-				muted>
-		</video>
-	</div> -->
+    <div class="carousel-item contents hover:scale-[102%] transition-all duration-300">
+        <video
+                class="w-[100%] sm:w-[52%] lg:w-[52%] xl:w-[52%] rounded-lg shadow-lg"
+                src="/python.mov"
+                autoplay
+                loop
+                muted
+                playsinline>
+        </video>
+    </div>
 </div>
